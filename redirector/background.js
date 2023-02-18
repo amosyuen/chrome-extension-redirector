c0dd4a2e082fc21fd32bec68e51fc0d9fa2cb2c0
Amos Yuen <amosyuen@gmail.com>
2023-02-18 10:08:58 -0800

    Redirect from bing to google after loading

--- redirector/background.js
+++ redirector/background.js
@@ -1,30 +1,18 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    var redirects, pattern, from, to, redirectUrl;
-    redirects = JSON.parse(localStorage.getItem('redirects') || '[]');
-    for (var i=0; i<redirects.length; i++) {
-      from = redirects[i][0];
-      to = redirects[i][1];
-      try {
-        pattern = new RegExp(from, 'ig');
-      } catch(err) {
-        //bad pattern
-        continue;
-      }
-      match = details.url.match(pattern);
-      if (match) {
-        redirectUrl = details.url.replace(pattern, to);
-        if (redirectUrl != details.url) {
-          return {redirectUrl: redirectUrl};
-        }
-      }
-    }
-    return {};
-  },
-  {
-    urls: [
-      "<all_urls>",
-    ],
-  },
-  ["blocking"]
+const MATCH_REGEX = new RegExp(
+  "^https://www.bing.com/search.*?[?&]q=([^&]+).*$"
 );
+const REPLACE_REGEX = "https://www.google.com/search?q=$1";
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status != "complete") {
+    return;
+  }
+  redirectUrl = tab.url.replace(MATCH_REGEX, REPLACE_REGEX);
+  if (redirectUrl == tab.url) {
+    return;
+  }
+
+  chrome.tabs.update(tabId, {
+    url: redirectUrl,
+  });
+});
